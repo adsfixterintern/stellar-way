5dd2071b9af647f7ef86bd52c8e451648800f476
Hasan Rafi Ahmed <hasan.cse570@gmail.com>
2026-03-16 09:55:51 +0600

    reserve now home

--- app/(withCommonLayout)/page.tsx
+++ app/(withCommonLayout)/page.tsx
@@ -1,9 +1,11 @@
 import FAQ from '@/components/home/FAQ'
+import Reserve from '@/components/home/Reserve'
 import React from 'react'
 
 export default function page() {
   return (
     <div>
+      <Reserve></Reserve>
       <FAQ></FAQ>
     </div>
   )

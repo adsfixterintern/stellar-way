a3613af719bb2c4faba2ecade45dd3305caf48e1
Hasan Rafi Ahmed <hasan.cse570@gmail.com>
2026-03-16 10:40:51 +0600

    exp section in home

--- app/(withCommonLayout)/page.tsx
+++ app/(withCommonLayout)/page.tsx
@@ -2,14 +2,14 @@ import FAQ from '@/components/home/FAQ'
 import Reserve from '@/components/home/Reserve'
 import React from 'react'
 import AboutSection from '../_components/AboutSection'
+import Exp from '@/components/home/Exp'
 
 export default function page() {
   return (
     <div>
+      <AboutSection />
+      <Exp></Exp>
       <Reserve></Reserve>
-
-
-      <AboutSection/>
       <FAQ></FAQ>
     </div>
   )

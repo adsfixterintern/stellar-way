9d1a21d3ac21845e5fa556b29a24b9f2de356863
HFsa-RaShid <hafsa20cse028@gmail.com>
2026-04-27 16:08:26 +0600

    update:offers

--- components/home/OfferSection.tsx
+++ components/home/OfferSection.tsx
@@ -256,7 +256,7 @@ const OfferCard = ({
       <div className="absolute top-6 left-6 z-10">
         <div className="bg-[#1A4E11] text-white px-5 py-2 rounded-2xl font-black text-xl shadow-lg">
           {offer.discountPercentage}%{" "}
-          <span className="text-[10px] block opacity-80 leading-none">Off</span>
+          <span className="text-[10px] block opacity-80 leading-none">Off (Dine-in Only)</span>
         </div>
       </div>
 
